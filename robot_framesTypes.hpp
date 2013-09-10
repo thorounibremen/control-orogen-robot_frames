1e78e99a8aaaf4099bb56b10d2cc50a1fa96ecb8
Malte Wirkus <malte.wirkus@dfki.de>
2013-09-10 16:47:39 +0200

    initial commit by rock-create-orogen

---
+++ robot_framesTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef robot_frames_TYPES_HPP
+#define robot_frames_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace robot_frames {
+}
+
+#endif
+
